c747730202211e31465e458746bdaf58086c1273
leonanthomaz <leonan.thomaz@gmail.com>
2024-03-23 23:16:41 -0300

    primeiro commit

---
+++ backend/src/main/java/app/vercel/leonanthomaz/jobexplorer/JobexplorerApplication.java
@@ -0,0 +1,13 @@
+package app.vercel.leonanthomaz.jobexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobexplorerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/vercel/leonanthomaz/jobexplorer/JobexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vercel.leonanthomaz.jobexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
